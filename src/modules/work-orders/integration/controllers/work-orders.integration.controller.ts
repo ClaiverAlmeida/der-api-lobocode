c59e6df6c2e6ca62f163e7465f171849506f865e
José Henrique <josehenriquefnbr@gmail.com>
2026-06-02 20:13:23 -0300

    fix: retirada herança do controller universal, retirando métodos indesejáveis para este controller, deixando apenas para leitura.

--- src/modules/work-orders/integration/controllers/work-orders.integration.controller.ts
+++ src/modules/work-orders/integration/controllers/work-orders.integration.controller.ts
@@ -1,8 +1,5 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { Public } from 'src/shared/auth/decorators/public.decorator';
-import { UniversalController } from 'src/shared/universal';
-import { CreateWorkOrderDto } from '../../dto/create-work-order.dto';
-import { UpdateWorkOrderDto } from '../../dto/update-work-order.dto';
 import {
   WorkOrderIntegrationIdParamDto,
   WorkOrderIntegrationSequentialParamDto,
@@ -14,23 +11,12 @@ import { WorkOrdersIntegrationService } from '../services/work-orders.integratio
 @Public()
 @UseGuards(WorkOrdersIntegrationRateLimitGuard, SharedTokenGuard)
 @Controller('integration/work-orders')
-export class WorkOrdersIntegrationController extends UniversalController<
-  CreateWorkOrderDto,
-  UpdateWorkOrderDto,
-  WorkOrdersIntegrationService
-> {
-  constructor(service: WorkOrdersIntegrationService) {
-    super(service);
-  }
+export class WorkOrdersIntegrationController {
+  constructor(private readonly service: WorkOrdersIntegrationService) {}
 
   @Get()
   buscarTodos() {
     return this.service.buscarTodos();
-  }
-
-  @Get(':id')
-  buscarPorIdIntegracao(@Param() { id }: WorkOrderIntegrationIdParamDto) {
-    return this.service.buscarPorId(id);
   }
 
   @Get('sequential/:sequentialNumber')
@@ -39,5 +25,10 @@ export class WorkOrdersIntegrationController extends UniversalController<
     { sequentialNumber }: WorkOrderIntegrationSequentialParamDto,
   ) {
     return this.service.buscarPorCampo('sequentialNumber', sequentialNumber);
+  }
+
+  @Get(':id')
+  buscarPorIdIntegracao(@Param() { id }: WorkOrderIntegrationIdParamDto) {
+    return this.service.buscarPorId(id);
   }
 }
